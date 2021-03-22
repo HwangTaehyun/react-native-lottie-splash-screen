03a494dce6b48257a0715fef0a9e6e773d54b4b5
Taehyun Hwang <eeht1717@gmail.com>
2021-03-22 15:10:57 +0900

    [feat] Add index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "react-native-splash-screen" {
+declare module "react-native-lottie-splash-screen" {
     export default class SplashScreen {
         static hide(): void;
         static show(): void;
