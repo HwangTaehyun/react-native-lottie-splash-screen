605f84967dcac185bf236adedca9a73b0b5d01af
Taehyun Hwang <eeht1717@gmail.com>
2022-07-09 02:51:59 +0900

    [update] Update Android example for react-native 0.69.1

--- examples/App.js
+++ examples/App.js
@@ -1,72 +1,59 @@
 /**
- * SplashScreen
- * 启动屏
- * from：http://www.devio.org
- * Author:CrazyCodeBoy
- * GitHub:https://github.com/crazycodeboy
- * Email:crazycodeboy@gmail.com
+ * LottieSplashScreen
+ * from: https://hwangtaehyun.github.io
+ * Author:TaehyunHwang
+ * GitHub:https://github.com/HwangTaehyun
+ * Email:eeht1717@gmail.com
  * @flow
  */
 'use strict';
 
+import React, {useEffect} from 'react';
+import LottieSplashScreen from 'react-native-lottie-splash-screen';
+import {StyleSheet, View, Text, TouchableOpacity, Linking} from 'react-native';
 
-import React, {Component} from 'react';
-import {
-    StyleSheet,
-    View,
-    Text,
-    TouchableOpacity,
-    Linking,
-} from 'react-native'
-import SplashScreen from 'react-native-splash-screen'
+const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      LottieSplashScreen.hide(); // here
+    }, 5_000);
+  }, []);
 
-export default class example extends Component {
-
-    componentDidMount() {
-        SplashScreen.hide();
-    }
-
-
-    render() {
-        return (
-            <TouchableOpacity
-                style={styles.container}
-                onPress={(e)=> {
-                    Linking.openURL('https://coding.imooc.com/class/304.html');
-                }}
-            >
-                <View >
-                    <Text style={styles.item}>
-                        SplashScreen 启动屏
-                    </Text>
-                    <Text style={styles.item}>
-                        @：http://www.devio.org/
-                    </Text>
-                    <Text style={styles.item}>
-                        GitHub:https://github.com/crazycodeboy
-                    </Text>
-                    <Text style={styles.item}>
-                        Email:crazycodeboy@gmail.com
-                    </Text>
-                </View>
-            </TouchableOpacity>
-        )
-    }
-
-}
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={e => {
+        Linking.openURL(
+          'https://github.com/HwangTaehyun/react-native-lottie-splash-screen',
+        );
+      }}>
+      <Text style={styles.item}>
+        Please star for react-native-lottie-splash-screen!
+      </Text>
+      <Text style={styles.item}>Click this screen!</Text>
+      <Text style={styles.item}>LottieSplashScreen</Text>
+      <Text style={styles.item}>@：https://hwangtaehyun.github.io</Text>
+      <Text style={styles.item}>GitHub: https://github.com/HwangTaehyun</Text>
+      <Text style={styles.item}>Email: eeht1717@gmail.com</Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f3f2f2',
-        marginTop: 30
-    },
-    item: {
-        fontSize: 20,
-    },
-    line: {
-        flex: 1,
-        height: 0.3,
-        backgroundColor: 'darkgray',
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  item: {
+    fontSize: 20,
+    marginBottom: 30,
+    color: 'black',
+  },
+  line: {
+    flex: 1,
+    height: 0.3,
+  },
+});
+
+export default App;
