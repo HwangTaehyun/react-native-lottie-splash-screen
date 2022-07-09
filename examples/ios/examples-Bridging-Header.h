86b1e5c812747a8fdafe635d89acb450a5afa6df
Taehyun Hwang <eeht1717@gmail.com>
2022-07-09 16:34:49 +0900

    [feat] Add lottie-ios & lottie-react-native package

--- examples/ios/examples-Bridging-Header.h
+++ examples/ios/examples-Bridging-Header.h
@@ -1,8 +1,7 @@
 // examples-Bridging-Header.h
 
 #ifndef examples_Bridging_Header_h
-#define examples_Bridging_Header_h
-
+#define examples_Bridging_Header_
 #import "RNSplashScreen.h" // here
 
 #endif /* examples_Bridging_Header_h */
