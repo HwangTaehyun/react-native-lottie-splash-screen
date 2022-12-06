c147793e3d069c805edb445a56a7ed8e7e5348b2
Jeeyoung <64334381+hancandice@users.noreply.github.com>
2022-12-06 22:36:03 +0100

    Modify setAnimationFinished function to reflect argument value

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -72,7 +72,7 @@ RCT_EXPORT_MODULE(SplashScreen)
 }
 
 + (void)setAnimationFinished:(Boolean)flag {
-    isAnimationFinished = true;
+    isAnimationFinished = flag;
     if (waiting) {
       dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)),
                      dispatch_get_main_queue(), ^{
